b11760cf7344c5eb2b42805c2c7a09eed6774c2f
zjt301 <2647259942@qq.com>
2020-06-18 16:52:38 +0800

    kypz

--- ERPAPI/Startup.cs
+++ ERPAPI/Startup.cs
@@ -40,7 +40,6 @@ namespace ERPAPI
                             .AllowCredentials();
                 });
             });
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
