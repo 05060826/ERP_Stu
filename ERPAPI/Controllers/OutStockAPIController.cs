0fbaeb7eeb85309ae062b158882e04cae5ba225f
11 <11>
2020-06-19 09:20:22 +0800

    out

--- ERPAPI/Controllers/OutStockAPIController.cs
+++ ERPAPI/Controllers/OutStockAPIController.cs
@@ -2,16 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Business;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Model;
+using Model.OutModel;
 
 namespace ERPAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api")]
     [ApiController]
     public class OutStockAPIController : ControllerBase
     {
+        Allot_Business _Business = null;
+        public OutStockAPIController()
+        {
+            _Business = new Allot_Business();
+        }
+        [Route("show")]
+        [HttpGet]
+        public List<OutStock> ShowPage()
+        {
+            var list = _Business.Select<OutStock>("select * from Clear a join Client b on a.ClientId=b.CLientId");
+            
+            return list;
+        }
+        [HttpPost]
+        public void ADD()
+        {
 
+        }
+        [HttpDelete]
+        public void Delete()
+        {
+
+        }
+        [HttpPut]
+        public void Update()
+        {
+
+        }
 
 
 
---
+++ Model/OutModel/OutStock.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.OutModel
+{
+    public class OutStock
+    {
+        public int ClearId { get; set; }
+        public string CleaNumber { get; set; }
+        public int ClientId { get; set; }
+        public int MId { get; set; }
+        public int SId { get; set; }
+        public int Number { get; set; }
+        public double SellMoney { get; set; }
+        public double Rate { get; set; }
+        public double Discount { get; set; }
+        public double SMoney { get; set; }
+        public string Addresses { get; set; }
+        public DateTime CTime { get; set; }
+        public int AId { get; set; }
+        public int CState { get; set; }
+        public int IsState { get; set; }
+
+
+        public int CLientId { get; set; }
+        public string ClientName { get; set; }
+
+
+
+    }
+}
